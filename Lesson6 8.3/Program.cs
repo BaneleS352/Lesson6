d3224fcaef98e992532f8625220931fee8a51041
BaneleS352 <BaneleS@betsoftware.com>
2026-03-23 13:34:23 +0200

    8.3
    8.3

---
+++ Lesson6 8.3/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+public class Program
+{
+    public class User
+    {
+        private string _username;
+
+        public User(string username)
+        {
+            _username = username;
+        }
+
+        public string Username
+        {
+            get
+            {
+                return _username;
+            }
+            set
+            {
+                if (string.IsNullOrWhiteSpace(value))
+                {
+                    Console.WriteLine("Username cannot be empty.");
+                    return;
+                }
+
+                _username = value;
+            }
+        }
+        public static void Main()
+        {
+            var user = new User("Witcher01");
+
+            Console.WriteLine($"Starting username: {user.Username}");
+
+            user.Username = "";
+            Console.WriteLine($"After invalid update: {user.Username}");
+
+            user.Username = "WhiteWolf";
+            Console.WriteLine($"After valid update: {user.Username}");
+        }
+
+    }
+
+}
