bdbd9e009dede24f58f06bb2f5c2fafac8d0b662
BaneleS352 <BaneleS@betsoftware.com>
2026-03-23 13:38:12 +0200

    8.4
    8.4

---
+++ Lesson6 8.4/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+
+public class Program
+{
+    public class Employee
+    {
+        public string Name { get; }
+
+        public Employee(string name)
+        {
+            Name = name;
+        }
+
+        public virtual void Work()
+        {
+            Console.WriteLine($"{Name} is working.");
+        }
+    }
+
+    public class Developer : Employee
+    {
+        public Developer(string name) : base(name) { }
+
+        public override void Work()
+        {
+            Console.WriteLine($"{Name} is writing code.");
+        }
+    }
+
+    public class Manager : Employee
+    {
+        public Manager(string name) : base(name) { }
+
+        public override void Work()
+        {
+            Console.WriteLine($"{Name} is managing the team.");
+        }
+    }
+
+    // TODO: Create an Intern class that inherits from Employee
+    // TODO: Override Work() to print:
+    // "{Name} is learning and helping with small tasks."
+    public class Intern : Employee
+    {
+        public Intern(string name) : base(name) { }
+
+        public override void Work()
+        {
+            Console.WriteLine($"{Name} is learning and helping with small tasks.");
+        }
+    }
+
+    public static void Main()
+    {
+        List<Employee> team = new()
+        {
+            new Developer("Ada"),
+            new Manager("Grace"),
+			// TODO: Add an Intern object here
+            new Intern("Sam")
+        };
+
+        foreach (var employee in team)
+        {
+            employee.Work();
+        }
+    }
+}
